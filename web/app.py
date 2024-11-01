4c06cd17b627d3b10133d8321717e6d4a20cbb08
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-11-01 20:24:02 -0300

    refactor: Adicionar documentação OpenAPI ao objeto FastAPI em web.app

--- web/app.py
+++ web/app.py
@@ -9,16 +9,21 @@ from web.config import (
 )
 from web.config.settings.base import Settings
 from web.di import Di
+from web.docs.api import api_info
 
 
 def create_app() -> FastAPI:
-    is_in_test = not is_app_in_production_mode()
+    is_in_test: bool = not is_app_in_production_mode()
     config_di(test=is_in_test)
 
     settings: Settings = Di.get_raw(Settings)
     app: FastAPI = FastAPI(
         title=settings.api_title,
         debug=is_in_test,
+        summary=api_info.summary,
+        version=api_info.version,
+        contact=api_info.contact,
+        license_info=api_info.license_info,
     )
 
     add_error_handlers(app)
