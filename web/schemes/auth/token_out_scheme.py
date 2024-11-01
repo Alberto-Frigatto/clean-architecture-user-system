65870e807bb9cbccf32ff8a4c03be34fd0a032d2
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-11-01 20:22:00 -0300

    refactor: Adicionar documentação OpenAPI na classe TokenOutScheme

--- web/schemes/auth/token_out_scheme.py
+++ web/schemes/auth/token_out_scheme.py
@@ -1,6 +1,15 @@
+from typing import Any
+
+from web.docs.examples.schemes.auth_schemes import TokenOutScheme_example
 from web.schemes.base import OutScheme
 
 
 class TokenOutScheme(OutScheme):
     access_token: str
     token_type: str = 'bearer'
+
+    model_config: dict[str, Any] = {  # type: ignore
+        'json_schema_extra': {
+            'examples': [TokenOutScheme_example],
+        }
+    }
