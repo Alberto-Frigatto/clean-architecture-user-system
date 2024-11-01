b06177379bff7fa72831498a865b7a5c23df9619
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-11-01 20:07:27 -0300

    refactor: Mover e alterar classes de resposta HTTP de erro
    Move as classes BadRequest, Conflict, Forbidden, Unauthorized, UnprocessableEntity para o pacote web.http_error_responses e simplifica seus construtores para satisfazer à classe HttpError.

---
+++ web/http_error_responses/unprocessable_entity.py
@@ -0,0 +1,18 @@
+from http import HTTPStatus
+
+from usecases.exceptions.base import AppException
+from web.http_error_responses.base import HttpError
+
+
+class UnprocessableEntity(HttpError):
+    def __init__(
+        self,
+        error: AppException,
+        *,
+        headers: dict[str, str] | None = None,
+    ) -> None:
+        super().__init__(
+            error=error,
+            headers=headers,
+            status=HTTPStatus.UNPROCESSABLE_ENTITY,
+        )
