2f0fcd0799603259a077ed62364b5bfd34918503
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-11-01 20:08:26 -0300

    chore: Adicionar imports ao pacote web.http_error_responses

---
+++ web/http_error_responses/__init__.py
@@ -0,0 +1,7 @@
+from .bad_request import BadRequest
+from .conflict import Conflict
+from .forbidden import Forbidden
+from .method_not_allowed import MethodNotAllowed
+from .not_found import NotFound
+from .unauthorized import Unauthorized
+from .unprocessable_entity import UnprocessableEntity
