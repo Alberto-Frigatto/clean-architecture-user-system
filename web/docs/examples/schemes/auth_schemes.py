808e62e7fb801d34b8fce004de663fcca756b8f5
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-11-01 20:21:25 -0300

    docs: Criar variáveis de documentação para schemas de autenticação

---
+++ web/docs/examples/schemes/auth_schemes.py
@@ -0,0 +1,6 @@
+from typing import Any
+
+TokenOutScheme_example: dict[str, Any] = {
+    'access_token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+    'token_type': 'bearer',
+}
