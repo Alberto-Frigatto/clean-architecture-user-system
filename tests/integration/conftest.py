e663dc7f3287cf705bfca473ba76d25c3e2b5ce2
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-11-01 13:16:33 -0300

    test: Criar fixture de usuário em tests/integration/conftest.py

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,8 +1,13 @@
 import os
 from collections.abc import AsyncGenerator
+from datetime import date
 
+import pytest
 import pytest_asyncio
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+
+from domain.entities import User
+from domain.value_objects import ColorTheme, Language
 
 
 @pytest_asyncio.fixture
@@ -21,3 +26,16 @@ async def motor_database() -> AsyncGenerator[AsyncIOMotorDatabase]:
         await db.drop_collection(collection_name)
 
     client.close()
+
+
+@pytest.fixture
+def user() -> User:
+    return User(
+        id='01JAZR9WGHX847Q382GV068JRS',
+        username='Alberto Frigatto',
+        email='alberto@gmail.com',
+        hashed_password='senha_criptografada',
+        birth_date=date(year=2005, month=2, day=27),
+        color_theme=ColorTheme.DARK,
+        language=Language.PT_BR,
+    )
