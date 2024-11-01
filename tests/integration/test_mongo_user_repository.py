a247e001c17a2d351b2149fbff3523afe94ad614
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-11-01 13:31:51 -0300

    test: Alterar testes para a classe MongoUserRepository

--- tests/integration/test_mongo_user_repository.py
+++ tests/integration/test_mongo_user_repository.py
@@ -5,6 +5,7 @@ import pytest
 import pytest_asyncio
 from motor.motor_asyncio import AsyncIOMotorDatabase
 
+from adapters.id import Ulid
 from adapters.repositories.user import MongoUserRepository
 from domain.entities import User
 from domain.value_objects import ColorTheme, Language
@@ -17,18 +18,6 @@ async def repository(
     yield MongoUserRepository(motor_database)
 
 
-@pytest.fixture
-def user() -> User:
-    return User(
-        username='Alberto Frigatto',
-        email='alberto@gmail.com',
-        hashed_password='senha_criptografada',
-        birth_date=date(year=2005, month=2, day=27),
-        color_theme=ColorTheme.DARK,
-        language=Language.PT_BR,
-    )
-
-
 @pytest.mark.asyncio
 async def test_create_user_success(
     repository: MongoUserRepository,
@@ -38,11 +27,12 @@ async def test_create_user_success(
 ) -> None:
     await repository.create(user)
 
-    created_user = await motor_database.users.find_one({'_id': user.id})
+    user_id_bytes: bytes = bytes(Ulid(user.id))
+    created_user = await motor_database.users.find_one({'_id': user_id_bytes})
 
     assert created_user is not None
     assert isinstance(created_user, dict)
-    assert created_user.get('_id') == user.id
+    assert created_user.get('_id') == user_id_bytes
     assert created_user.get('username') == user.username
     assert created_user.get('email') == user.email
     assert created_user.get('hashed_password') == user.hashed_password
@@ -149,11 +139,13 @@ async def test_update_user_success(
 
     await repository.update(new_user)
 
-    updated_user = await motor_database.users.find_one({'_id': new_user.id})
+    updated_user = await motor_database.users.find_one(
+        {'_id': bytes(Ulid(new_user.id))}
+    )
 
     assert updated_user is not None
     assert isinstance(updated_user, dict)
-    assert updated_user.get('_id') == new_user.id == user.id
+    assert str(Ulid(updated_user.get('_id'))) == new_user.id == user.id
     assert updated_user.get('username') == new_user.username
     assert updated_user.get('email') == new_user.email
     assert updated_user.get('hashed_password') == new_user.hashed_password
