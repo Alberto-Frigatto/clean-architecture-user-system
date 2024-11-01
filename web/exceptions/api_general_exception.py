5b1d47f9c9ed4573b95af719acc366153d190f3f
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-11-01 19:58:57 -0300

    feat: Criar classe MethodNotAllowed dentro da classe ApiGeneralException

--- web/exceptions/api_general_exception.py
+++ web/exceptions/api_general_exception.py
@@ -5,3 +5,9 @@ class ApiGeneralException:
     class EndpointNotFound(ApiException):
         def __init__(self, endpoint: str) -> None:
             super().__init__(message=f'O endpoint {endpoint} não existe')
+
+    class MethodNotAllowed(ApiException):
+        def __init__(self, *, method: str, endpoint: str) -> None:
+            super().__init__(
+                message=f'O método {method} não é permitido para o endpoint {endpoint}'
+            )
