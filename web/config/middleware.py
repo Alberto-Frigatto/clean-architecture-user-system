aa57d91902b7817af03b83b67fa9a777d638a62d
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-11-03 20:18:04 -0300

    refactor: Adicionar tipagem correta nas variáveis de web.config.middleware

--- web/config/middleware.py
+++ web/config/middleware.py
@@ -1,6 +1,6 @@
 import json
 import time
-from collections.abc import Callable
+from collections.abc import AsyncGenerator, Callable
 from typing import Any
 
 from fastapi import FastAPI, Request, Response, UploadFile
@@ -31,14 +31,18 @@ def add_middlewares(app: FastAPI, *, test: bool) -> None:
             response: Response = await call_next(request)
             process_time: float = time.perf_counter() - start_time
 
-            response_body: Any | None = None
+            response_body: dict[str, str | Any] | None = None
 
             try:
-                response_body = [section async for section in response.body_iterator]  # type: ignore
-                response.body_iterator = iterate_in_threadpool(iter(response_body))  # type: ignore
-                response_body = json.loads(b"".join(response_body))
+                response_body_data: list[bytes] = [
+                    section async for section in response.body_iterator  # type: ignore
+                ]
+                response.body_iterator: AsyncGenerator = iterate_in_threadpool(  # type: ignore
+                    iter(response_body_data)
+                )
+                response_body = json.loads(b"".join(response_body_data))
             except json.decoder.JSONDecodeError:
-                response_body = None
+                pass
 
             HttpLogger(
                 request_headers=dict(request.headers),
