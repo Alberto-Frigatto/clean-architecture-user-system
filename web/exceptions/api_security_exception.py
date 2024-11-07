ecc52861963df2f56e8ebf74fc85187963cc2f18
Alberto Frigatto <albertofrigattoferreira@gmail.com>
2024-11-07 13:20:37 -0300

    refactor: Traduzir mensagens das exceções da classe ApiSecurityException

--- web/exceptions/api_security_exception.py
+++ web/exceptions/api_security_exception.py
@@ -4,12 +4,12 @@ from web.exceptions.base import ApiException
 class ApiSecurityException:
     class MissingJwt(ApiException):
         def __init__(self) -> None:
-            super().__init__(message='Token JWT não fornecido')
+            super().__init__(message='JWT token not provided')
 
     class InvalidJwt(ApiException):
         def __init__(self) -> None:
-            super().__init__(message='Token JWT inválido')
+            super().__init__(message='Invalid JWT token')
 
     class ExpiredJwt(ApiException):
         def __init__(self) -> None:
-            super().__init__(message='Token JWT expirado')
+            super().__init__(message='Expired JWT token')
